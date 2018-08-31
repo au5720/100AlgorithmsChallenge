d7ddcb6f39cc958b6de21e9462f47f65674feeba
au5720 <jennifermorgan2001@gmail.com>
2018-08-31 17:44:00 +0100

    Before push

--- addBorder/addBorder.js
+++ addBorder/addBorder.js
@@ -1,5 +1,10 @@
 function addBorder(picture) {
-
+    var frame = [];
+    var headTail = '*'.repeat(picture.length+2);
+    frame.push( headTail );
+    frame = frame.concat( picture.map(v => '*' + v + '*'));
+    frame.push( headTail );
+    return frame;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
