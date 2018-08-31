6cd3fa02a1163b78b3be971607f4b268629ee442
au5720 <jennifermorgan2001@gmail.com>
2018-08-31 18:27:32 +0100

    VS Code is giving me issues

--- adjacentElementsProduct/adjacentElementsProduct.js
+++ adjacentElementsProduct/adjacentElementsProduct.js
@@ -1,5 +1,6 @@
-function adjacentElementsProduct(inputArray: number[]): number {
-
+function adjacentElementsProduct(inputArray) {
+    var pairs=inputArray.map( (a,b) => [a,inputArray[b+1]]).filter( v => v[1] !== undefined);
+    return pairs.map( (v,i) => [v[0] * v[1],i]).sort( (a,b) => a[0] < b[0])[0][0];
 }
 
 console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
