116c5b1c544b8f0f4b7ab67f03cf3d5866d00ee2
au5720 <jennifermorgan2001@gmail.com>
2018-08-31 16:00:49 +0100

    Supposedly the formula is to find the median?

--- absoluteValuesSumMinization/absoluteValuesSumMinization.js
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.js
@@ -1,8 +1,42 @@
-function absoluteValuesSumMinimization(a: number[]): number {
+function absoluteValuesSumMinimization(a){
+   a.sort();
+   var total=0;
+   var results=[];
+   console.log(a);
+   for(var i=0; i < a.length; i++) {
+    for(var ii=0; ii < a.length; ii++) {
+        var aa=a[ii];
+        var v=a[i];
+        var vv=Math.abs(aa-v);
+        console.log(aa + ' ' + v + ' ' + vv);
+        total=total + Math.abs(a[ii] - a[i]);
+    }
+    console.log(total);
+    console.log('--------------------------');
+    results.push(total);
+    total=0
+   }
 
+   results.sort();
+   console.log(results);
+   return results[0];
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+function assertEqual(actual, expected, testName) {
+    if(actual === expected) {
+        console.log('passed - ' + testName);
+    } else {
+        console.log('failed - ' + testName + ' Expected: ' + expected + ' but got ' + actual);
+    }
+}
+//console.log(absoluteValuesSumMinimization([2, 4, 7]));
+assertEqual(absoluteValuesSumMinimization([2, 4, 7]), 4, 'test 1');
+
+//console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+//assertEqual(absoluteValuesSumMinimization([2, 4, 7, 6]), 4, 'test 2');
+
 // console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+//assertEqual(absoluteValuesSumMinimization([2, 4, 7, 6, 6]), 7, 'test 3');
+
 // console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+//assertEqual(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]), 7, 'test 4');
