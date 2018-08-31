1e3975b7429eff85a7e1bb21bf5482e54a326091
au5720 <jennifermorgan2001@gmail.com>
2018-08-31 21:52:50 +0100

    alphaShift tested

--- alphabeticShift/alphabeticShift.js
+++ alphabeticShift/alphabeticShift.js
@@ -1,5 +1,13 @@
-function alphabeticShift(inputString: string): string {
-
+function alphabeticShift(inputString) {
+    var str='';
+    for(var i=0; i < inputString.length; i++) {
+        var num=inputString[i].charCodeAt(0) + 1;
+        if(num > 122) {
+            str = str + String.fromCharCode(97);
+        } else {
+            str = str + String.fromCharCode(num);
+        }
+    }
 }
 
 console.log(alphabeticShift('crazy'));
