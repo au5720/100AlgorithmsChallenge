e21e62e2e7b9867d51421f6a519c3be012fbe73a
au5720 <jennifermorgan2001@gmail.com>
2018-09-09 14:54:53 +0100

    Took a while to understand the meaning of the question

--- areSimilar/areSimilar.js
+++ areSimilar/areSimilar.js
@@ -1,7 +1,24 @@
-function areSimilar(a, b) {
-   
+function areSimilar(a,b) {
+    var c=[];
+    var d=[];
+
+    if(a.toString() === b.toString()) {
+        return true;
+    }
+    for(var i=0; i < a.length; i++) {
+        if(a[i] !== b[i]) {
+            c.push(a[i]);
+            d.push(b[i]);
+        }
+    }
+    if(c.toString() === d.reverse().toString()) {
+        return true;
+    }
+    return false;
 }
 
-console.log(areSimilar([1, 2, 3], [1, 2, 3]));
+console.log(areSimilar([1,2,3], [1,2,3]));
 console.log(areSimilar([1, 2, 3], [2, 1, 3]));
-console.log(areSimilar([1, 2, 2], [2, 1, 1]));
+console.log(areSimilar([2, 1, 3], [1, 2, 3]));
+
+
