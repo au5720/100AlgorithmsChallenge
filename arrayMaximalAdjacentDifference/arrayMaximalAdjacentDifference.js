73398340a993a2ecf3689467599956780a3b83ae
au5720 <jennifermorgan2001@gmail.com>
2018-09-09 15:47:11 +0100

    some Function currying

--- arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.js
+++ arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.js
@@ -1,5 +1,9 @@
-function arrayMaximalAdjacentDifference(inputArray: number[]): number {
-
+function arrayMaximalAdjacentDifference(inputArray) {
+    return inputArray.map( (v, i, a) => Math.abs( v - a[i-1] ) )
+                    .filter(Number.isInteger)
+                    .sort()
+                    .reverse()[0];
 }
 
 console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
+
