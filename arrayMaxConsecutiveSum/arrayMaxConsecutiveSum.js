7dd2a4ff28507c01aa5de658149eef600d22fd54
au5720 <jennifermorgan2001@gmail.com>
2018-09-09 15:33:58 +0100

    Simple enough

--- arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.js
+++ arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.js
@@ -1,5 +1,17 @@
-function arrayMaxConsecutiveSum(inputArray: number[], k: number): number {
-
+function arrayMaxConsecutiveSum(inputArray, k) {
+    var answers=[];
+    var currentSum=0;
+    for(var x=0; x < inputArray.length; x++) {
+        if(( x + k ) <= inputArray.length) {
+            for(var y=x; y < (x + k); y++) {
+                currentSum+=inputArray[y];
+            }
+            answers.push(currentSum);
+            currentSum=0;
+        }
+    }
+    answers.sort().reverse()
+    return answers[0];
 }
 
 console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
